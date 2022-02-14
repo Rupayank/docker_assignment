7fbc5690897d9d5960078f0ba519dfa261185869
Rupayan <59760234+Rupayank@users.noreply.github.com>
2022-02-15 02:10:26 +0530

    CRUD to be checked

---
+++ db.js
@@ -0,0 +1,12 @@
+const POOL = require("pg").Pool;
+const pool = new POOL({
+	user: process.env.USER,
+	password: process.env.PASS,
+	database: process.env.DB,
+	host: "localhost",
+	port: process.env.PGPORT,
+});
+pool.on("connect", () => {
+	console.log("Connected to DB!");
+});
+module.exports = pool;
--- index.js
+++ index.js
@@ -1,7 +1,60 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 5000;
+const db = require("./db");
+const { v4: uuidv4 } = require("uuid");
+const port = process.env.PORT || 9000;
 app.listen(port, () => console.log(`Listening on port no. ${port}`));
-app.get("/", (req, res) => {
-	res.send("Hi There!!!");
+app.use(express.json());
+app.post("/todo", async (req, res) => {
+	try {
+		const { description, dueDate, dueTime, timeZone } = req.body;
+		const id = uuidv4();
+		const newTodo = await db.query(
+			"INSERT INTO public.todoInfo VALUES ($1,$2,$3,$4,$5) RETURNING *",
+			[id, description, dueDate, dueTime, timeZone]
+		);
+		res.status(200).send(newTodo.rows[0]);
+	} catch (err) {
+		console.log(err);
+	}
+});
+app.get("/todo", async (req, res) => {
+	try {
+		const allTodo = await db.query("SELECT * FROM public.todoInfo");
+		res.status(200).send(allTodo.rows);
+	} catch (err) {
+		console.log(err);
+	}
+});
+app.get("/todo/:id", async (req, res) => {
+	try {
+		const todo = await db.query("SELECT * FROM public.todoInfo WHERE id=$1", [
+			req.params.id,
+		]);
+		res.status(200).send(todo.rows);
+	} catch (err) {
+		console.log(err);
+	}
+});
+app.put("/todo/:id", async (req, res) => {
+	try {
+		const updateTodo = await db.query(
+			"UPDATE public.todoInfo SET branch=$1 WHERE id=$2",
+			[req.body.branch, req.params.id]
+		);
+		res.status(200).send({ message: "Branch updated" });
+	} catch (err) {
+		console.log(err);
+	}
+});
+app.delete("/todo/:id", async (req, res) => {
+	try {
+		const todo = await db.query("DELETE FROM public.todoInfo WHERE id=$1", [
+			req.params.id,
+		]);
+		res.status(200).send({ message: "Deleted" });
+	} catch (err) {
+		console.log(err);
+	}
 });
